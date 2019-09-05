5f7bb93d74712b148869eada3da58f9240773af3
sofiakarb <karvoun@di.uoa.gr>
2019-09-05 19:14:06 +0300

    fix way of getting keys from Consul

--- backup.sh
+++ backup.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 
 backup () {
+    #This takes the 2 values of workers
+
+
+    result="[\"datasets/myMaster","worker/myW","wo/3\"]"    #curl -s http://192.168.99.100:8500/v1/kv/?keys
+
+    if [[ -z "${result}" ]]; then
+        echo "no keys? what is that even mean?"
+    fi
+
+    if [[ ${result} != *","* ]]; then
+        key=$(echo ${result} | cut -d'"' -f 2)
+        echo ${key}
+    else
+        key=$(echo ${result} | cut -d',' -f 1)
+        name=$(echo ${key} | cut -d '"' -f 2)
+        echo ${name}
+
+        n=1
+        while true
+        do
+            n=$((${n} + 1))
+            other_half=$(echo ${result} | cut -d',' -f $n)
+            if [[ -z ${other_half} ]]; then
+                break
+            fi
+            name=$(echo ${other_half} | cut -d'"' -f 1)
+            echo ${name}
+        done
+
+    fi
+
+    exit 1
+    rm backup.json
 
     key="master/myMaster"
     #value=$(curl -s http://192.168.99.100/v1/kv/${EXAREME_MASTER_PATH}/$(curl -s ${CONSULURL}/v1/kv/${EXAREME_MASTER_PATH}/?keys | jq -r '.[]' | sed "s/${EXAREME_MASTER_PATH}\///g")?raw)
@@ -19,20 +52,19 @@ do
     if [[ -s backup.json  ]]; then       #file not empty
         while read -r line ; do
             key=$( echo "$line" | cut -d'=' -f 1)
-            value2=$( echo "$line" | cut -d'=' -f 2)
-            echo $key
-            echo $value2
-            #value=$(curl -s http://192.168.99.100/v1/kv/$key?raw)
-            value="10.20.30.9"
-            if [[ -z "$value" ]]; then      #kapote eixe timi. ara ekane restart to service
-                curl -s -X PUT -d @- http://192.168.99.100/v1/kv/${key} <<< ${value2}
-            elif [[ "$value" == "$value2" ]]; then
+            file_value=$( echo "$line" | cut -d'=' -f 2)
+            #kv_value=$(curl -s http://192.168.99.100/v1/kv/$key?raw)
+            kv_value="10.20.30.9"
+            if [[ -z "$kv_value" ]]; then      #kapote eixe timi. ara ekane restart to service
+                echo "service restared"
+                curl -s -X PUT -d @- http://192.168.99.100/v1/kv/${key} <<< ${file_value}
+            elif [[ "kv_value" == "file_value" ]]; then
                 echo "nothing happens"
             else
                 echo "need replace"
-                sed -i '/'$key'/ s/'$value2'/'$value'/' backup.json
+                sed -i '/'$key'/ s/'$file_value'/'kv_value'/' backup.json
             fi
-        done < backup.json
+        done < backup.json      #ana takta xronika diastimata ksana backup. alla pote to backup thelei prosoxi min ginei backup kai to consul exei molis kanei restart
     else
         touch backup.json
         backup
