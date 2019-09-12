436cb74657518f23a0743842bc8a352bcba9d8f0
sofiakarb <karvoun@di.uoa.gr>
2019-09-12 16:34:26 +0300

    after execution delete unwanted file

--- case3/copy_backup_file.sh
+++ case3/copy_backup_file.sh
@@ -4,3 +4,4 @@
 docker-machine scp myMaster1:/home/myMaster/backup/backup.json .
 docker-machine scp backup.json myMaster2:/home/myMaster/backup/
 docker-machine scp backup.json myMaster3:/home/myMaster/backup/
+rm backup.json
